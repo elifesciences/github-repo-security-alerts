3cfaa57c4bf9d51f29ddb6c895760edc13bc8f35
Luke <lsh-0@users.noreply.github.com>
2023-05-29 18:41:12 +0930

    added support for a project-maintainers.json file.

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 Prints a list of summarised Dependabot security alerts that are open, grouped by repository name.
 
+Accepts a map of `project-name => list-of-maintainers` as a JSON file to further group by maintainer. 
+See [maintainers-txt](https://github.com/elifesciences/maintainers-txt)
+
 ## Dependencies
 
 * Go 1.20+
@@ -10,57 +13,112 @@ Prints a list of summarised Dependabot security alerts that are open, grouped by
 
 ## Installation
 
-    git clone github-repo-security-alerts
+    git clone https://github.com/elifesciences/github-repo-security-alerts
     cd github-repo-security-alerts
     go build .
 
 ## Usage
 
     GITHUB_TOKEN=your-github-token ./github-repo-security-alerts
-    
+
+or
+
+    GITHUB_TOKEN=your-github-token ./github-repo-security-alerts project-maintainers.json
+
+with `project-maintainers.json` looking like:
+
+```json
+{
+  "project-foo": [
+    "jane"
+  ],
+  "project-baz": [
+    "jack",
+    "john"
+  ],
+}
+```
+
 ## Example
 
 ```json
 {
-  "api-dummy": [
-    {
-      "AgeDays": 19,
-      "Summary": "Improper header name validation in guzzlehttp/psr7",
-      "URL": "https://github.com/elifesciences/api-dummy/security/dependabot/13",
-      "CVE_ID": "CVE-2023-29197",
-      "GHSA_ID": "GHSA-wxmh-65f7-jcvw"
-    }
-  ],
-  "data-science-dags": [
+  "project-foo": [
     {
       "AgeDays": 6,
       "Summary": "Flask vulnerable to possible disclosure of permanent session cookie due to missing Vary: Cookie header",
-      "URL": "https://github.com/elifesciences/data-science-dags/security/dependabot/28",
+      "URL": "https://github.com/elifesciences/project-foo/security/dependabot/28",
       "CVE_ID": "CVE-2023-30861",
       "GHSA_ID": "GHSA-m2qf-hxjv-5gpq"
     },
   ],
-  "hypothesis-dummy": [
+  "project-bar": [
     {
       "AgeDays": 19,
       "Summary": "Improper header name validation in guzzlehttp/psr7",
-      "URL": "https://github.com/elifesciences/hypothesis-dummy/security/dependabot/4",
+      "URL": "https://github.com/elifesciences/project-bar/security/dependabot/4",
       "CVE_ID": "CVE-2023-29197",
       "GHSA_ID": "GHSA-wxmh-65f7-jcvw"
     }
   ],
-  "lens-s3": [
+  "project-baz": [
     {
       "AgeDays": 37,
       "Summary": "Potential XSS vulnerability in jQuery",
-      "URL": "https://github.com/elifesciences/lens-s3/security/dependabot/1",
+      "URL": "https://github.com/elifesciences/project-baz/security/dependabot/1",
       "CVE_ID": "CVE-2020-11022",
       "GHSA_ID": "GHSA-gxr4-xjj5-5px2"
     }
   ]
 }
 ```
-    
+
+and with a `project-maintainers.json` file:
+
+```
+{
+  "jack": {
+    "project-baz": [
+      {
+        "AgeDays": 59,
+        "Summary": "Potential XSS vulnerability in jQuery",
+        "URL": "https://github.com/elifesciences/project-baz/security/dependabot/1",
+        "CVE_ID": "CVE-2020-11022",
+        "GHSA_ID": "GHSA-gxr4-xjj5-5px2"
+      }
+    ]
+  },
+  "john": {
+    "project-baz": [
+      {
+        "AgeDays": 59,
+        "Summary": "Potential XSS vulnerability in jQuery",
+        "URL": "https://github.com/elifesciences/project-baz/security/dependabot/1",
+        "CVE_ID": "CVE-2020-11022",
+        "GHSA_ID": "GHSA-gxr4-xjj5-5px2"
+      }
+    ]
+  },
+  "jane": {
+    "project-foo": [
+      {
+        "AgeDays": 18,
+        "Summary": "Apache Airflow vulnerable to Privilege Context Switching Error",
+        "URL": "https://github.com/elifesciences/project-foo/security/dependabot/13",
+        "CVE_ID": "CVE-2023-25754",
+        "GHSA_ID": "GHSA-jchm-fm4q-c2fp"
+      },
+      {
+        "AgeDays": 21,
+        "Summary": "Apache Airflow vulnerable to stored Cross-site Scripting",
+        "URL": "https://github.com/elifesciences/project-foo/security/dependabot/12",
+        "CVE_ID": "CVE-2023-29247",
+        "GHSA_ID": "GHSA-vcf6-3wv2-5vcr"
+      }
+    ]
+}
+```
+
 ## Licence
 
 Copyright © 2023 eLife Sciences
--- main.go
+++ main.go
@@ -22,6 +22,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"math"
 	"net/url"
 	"os"
@@ -32,35 +33,23 @@ import (
 	"golang.org/x/oauth2"
 )
 
-func panicOnErr(err error, action string) {
-	if err != nil {
-		panic(fmt.Sprintf("failed with '%s' while '%s'", err.Error(), action))
-	}
-}
+// type aliases, just to help readability
+type Project = string
+type Maintainer = string
 
-func github_token() string {
-	token, present := os.LookupEnv("GITHUB_TOKEN")
-	if !present {
-		panic("envvar GITHUB_TOKEN not set.")
-	}
-	return token
-}
-
-// "https://github.com/elifesciences/journal-cms/security/dependabot/19" => "journal-cms"
-func extract_project_from_url(github_url string) string {
-	u, err := url.Parse(github_url)
-	panicOnErr(err, "parsing a URL")
-	p := u.Path
-	bits := strings.Split(p, "/")
-	return bits[2]
-}
-
+// subset of interesting fields from a github.DependabotAlert struct
 type Alert struct {
 	AgeDays int
 	Summary string
 	URL     string
 	CVE_ID  string
 	GHSA_ID string
+}
+
+func panicOnErr(err error, action string) {
+	if err != nil {
+		panic(fmt.Sprintf("failed with '%s' while '%s'", err.Error(), action))
+	}
 }
 
 func as_json(thing interface{}) string {
@@ -71,12 +60,36 @@ func as_json(thing interface{}) string {
 	return out.String()
 }
 
-func main() {
-	token := github_token()
-	org_name := "elifesciences"
+// ---
 
-	// ---
+func github_token() string {
+	token, present := os.LookupEnv("GITHUB_TOKEN")
+	if !present {
+		panic("envvar GITHUB_TOKEN not set.")
+	}
+	return token
+}
 
+// "https://github.com/elifesciences/journal-cms/security/dependabot/19" => "journal-cms"
+func extract_project_from_url(github_url string) Project {
+	u, err := url.Parse(github_url)
+	panicOnErr(err, "parsing a URL")
+	p := u.Path
+	bits := strings.Split(p, "/")
+	return bits[2]
+}
+
+func parse_maintainer_alias_map(args []string) map[Project][]Maintainer {
+	maintainer_alias_map := map[Project][]Maintainer{}
+	if len(args) > 0 {
+		path := args[0]
+		txt, _ := ioutil.ReadFile(path)
+		json.Unmarshal(txt, &maintainer_alias_map)
+	}
+	return maintainer_alias_map
+}
+
+func fetch_alert_list(org_name, token string) map[Project][]Alert {
 	ctx := context.Background()
 	ts := oauth2.StaticTokenSource(
 		&oauth2.Token{AccessToken: token},
@@ -89,33 +102,72 @@ func main() {
 	opts := &github.ListAlertsOptions{
 		State: &state,
 	}
-
-	alert_list, _, err := client.Dependabot.ListOrgAlerts(ctx, org_name, opts)
+	dependabot_alert_list, _, err := client.Dependabot.ListOrgAlerts(ctx, org_name, opts)
 	panicOnErr(err, "listing org security alerts")
 
 	now := time.Now()
-	idx := map[string][]Alert{}
-	for _, alert := range alert_list {
-		pname := extract_project_from_url(alert.GetHTMLURL())
+	idx := map[Project][]Alert{}
+	for _, dependabot_alert := range dependabot_alert_list {
+		pname := extract_project_from_url(dependabot_alert.GetHTMLURL())
 
 		project_alert_list, present := idx[pname]
 		if !present {
 			project_alert_list = []Alert{}
 		}
 
-		age := now.Sub(alert.GetCreatedAt().Time)
+		age := now.Sub(dependabot_alert.GetCreatedAt().Time)
 		age_days := int(math.Ceil(age.Hours() / 24))
-
-		a := Alert{
-			CVE_ID:  alert.SecurityAdvisory.GetCVEID(),
-			GHSA_ID: alert.SecurityAdvisory.GetGHSAID(),
-			Summary: alert.SecurityAdvisory.GetSummary(),
-			URL:     alert.GetHTMLURL(),
+		alert := Alert{
+			CVE_ID:  dependabot_alert.SecurityAdvisory.GetCVEID(),
+			GHSA_ID: dependabot_alert.SecurityAdvisory.GetGHSAID(),
+			Summary: dependabot_alert.SecurityAdvisory.GetSummary(),
+			URL:     dependabot_alert.GetHTMLURL(),
 			AgeDays: age_days,
 		}
-		project_alert_list = append(project_alert_list, a)
+		project_alert_list = append(project_alert_list, alert)
 		idx[pname] = project_alert_list
 	}
 
-	fmt.Println(as_json(idx))
+	return idx
+}
+
+func main() {
+	args := os.Args[1:]
+
+	token := github_token()
+	org_name := "elifesciences"
+
+	maintainer_alias_map := parse_maintainer_alias_map(args)
+	project_alert_map := fetch_alert_list(org_name, token)
+
+	if len(project_alert_map) > 0 && len(maintainer_alias_map) > 0 {
+		// we have project alerts and we have project maintainers.
+		// group the projects by maintainers.
+		// maintainer=>project=>[]Alert
+		maintainer_project_map := map[Maintainer]map[Project][]Alert{}
+		for project, alert_list := range project_alert_map {
+			project_maintainer_list, present := maintainer_alias_map[project]
+			if !present {
+				// project has no maintainers!
+				// it's possible the repository is new but using vulnerable deps.
+				// projects with no maintainers are handled in `maintainers-txt` project.
+				fmt.Fprintf(os.Stderr, "skipping project '%s' with %d alert(s): no maintainers found\n", project, len(alert_list))
+				continue
+			}
+			for _, maintainer := range project_maintainer_list {
+				project_map, present := maintainer_project_map[maintainer]
+				if !present {
+					project_map = map[Project][]Alert{}
+				}
+				project_map[project] = alert_list
+				maintainer_project_map[maintainer] = project_map
+			}
+		}
+		fmt.Println(as_json(maintainer_project_map))
+
+	} else if len(project_alert_map) > 0 {
+		// we have project alerts but no list of project maintainers.
+		// output everything as-is
+		fmt.Println(as_json(project_alert_map))
+	}
 }
